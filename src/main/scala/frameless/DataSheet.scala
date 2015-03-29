ce4a3be2e7fc0a3acc1af1257d207c235a5c71ab
Adelbert Chang <adelbertc@gmail.com>
2015-03-28 21:18:33 -0800

    Use LabelledGeneric to track schema

--- src/main/scala/frameless/DataSheet.scala
+++ src/main/scala/frameless/DataSheet.scala
@@ -11,22 +11,23 @@ import scala.collection.JavaConverters._
 import scala.reflect.ClassTag
 import scala.reflect.runtime.universe.TypeTag
 
-import shapeless.{ Generic, HList }
+import shapeless.{ Generic, HList, LabelledGeneric }
 import shapeless.ops.hlist.Prepend
+import shapeless.ops.record.Values
 import shapeless.ops.traversable.FromTraversable
 import shapeless.syntax.std.traversable._
 
-/** Wrapper around [[org.apache.spark.sql.DataFrame]] using [[shapeless.HList]]s to track schema.
+/** Wrapper around [[org.apache.spark.sql.DataFrame]] where the type parameter tracks the schema.
   *
   * All heavy-lifting is still being done by the backing DataFrame so this API will more or less
   * be 1-to-1 with that of the DataFrame's.
   */
-final class DataSheet[L <: HList] private(val dataFrame: DataFrame) {
+final class DataSheet[Schema <: HList] private(val dataFrame: DataFrame) {
   import DataSheet._
 
-  def as(alias: Symbol): DataSheet[L] = DataSheet(dataFrame.as(alias))
+  def as(alias: Symbol): DataSheet[Schema] = DataSheet(dataFrame.as(alias))
 
-  def as(alias: String): DataSheet[L] = DataSheet(dataFrame.as(alias))
+  def as(alias: String): DataSheet[Schema] = DataSheet(dataFrame.as(alias))
 
   def cache(): this.type = {
     dataFrame.cache()
@@ -35,24 +36,25 @@ final class DataSheet[L <: HList] private(val dataFrame: DataFrame) {
 
   def count(): Long = dataFrame.count()
 
-  def distinct: DataSheet[L] = DataSheet(dataFrame.distinct)
+  def distinct: DataSheet[Schema] = DataSheet(dataFrame.distinct)
 
-  def except(other: DataSheet[L]): DataSheet[L] =
+  def except(other: DataSheet[Schema]): DataSheet[Schema] =
     DataSheet(dataFrame.except(other.dataFrame))
 
   def explain(): Unit = dataFrame.explain()
 
   def explain(extended: Boolean): Unit = dataFrame.explain(extended)
 
-  def intersect(other: DataSheet[L]): DataSheet[L] =
+  def intersect(other: DataSheet[Schema]): DataSheet[Schema] =
     DataSheet(dataFrame.intersect(other.dataFrame))
 
   def isLocal: Boolean = dataFrame.isLocal
 
-  def join[M <: HList, Out <: HList](right: DataSheet[M])(implicit P: Prepend.Aux[L, M, Out]): DataSheet[Out] =
+  def join[OtherSchema <: HList, NewSchema <: HList](right: DataSheet[OtherSchema])(
+                                                     implicit P: Prepend.Aux[Schema, OtherSchema, NewSchema]): DataSheet[NewSchema] =
     DataSheet(dataFrame.join(right.dataFrame))
 
-  def limit(n: Int): DataSheet[L] = DataSheet(dataFrame.limit(n))
+  def limit(n: Int): DataSheet[Schema] = DataSheet(dataFrame.limit(n))
 
   def persist(newLevel: StorageLevel): this.type = {
     dataFrame.persist(newLevel)
@@ -70,12 +72,12 @@ final class DataSheet[L <: HList] private(val dataFrame: DataFrame) {
 
   def registerTempTable(tableName: String): Unit = dataFrame.registerTempTable(tableName)
 
-  def repartition(numPartitions: Int): DataSheet[L] = DataSheet(dataFrame.repartition(numPartitions))
+  def repartition(numPartitions: Int): DataSheet[Schema] = DataSheet(dataFrame.repartition(numPartitions))
 
-  def sample(withReplacement: Boolean, fraction: Double): DataSheet[L] =
+  def sample(withReplacement: Boolean, fraction: Double): DataSheet[Schema] =
     DataSheet(dataFrame.sample(withReplacement, fraction))
 
-  def sample(withReplacement: Boolean, fraction: Double, seed: Long): DataSheet[L] =
+  def sample(withReplacement: Boolean, fraction: Double, seed: Long): DataSheet[Schema] =
     DataSheet(dataFrame.sample(withReplacement, fraction, seed))
 
   def save(source: String, mode: SaveMode, options: Map[String, String]): Unit = dataFrame.save(source, mode, options)
@@ -113,7 +115,7 @@ final class DataSheet[L <: HList] private(val dataFrame: DataFrame) {
 
   override def toString(): String = s"DataSheet:\n${dataFrame.toString}"
 
-  def unionAll(other: DataSheet[L]): DataSheet[L] =
+  def unionAll(other: DataSheet[Schema]): DataSheet[Schema] =
     DataSheet(dataFrame.unionAll(other.dataFrame))
 
   def unpersist(): this.type = {
@@ -128,61 +130,102 @@ final class DataSheet[L <: HList] private(val dataFrame: DataFrame) {
 
   /////////////////////////
 
-  def collect[P <: Product]()(implicit Gen: Generic.Aux[P, L], P: ClassTag[P], L: FromTraversable[L]): Array[P] =
-    dataFrame.collect().map(unsafeRowToProduct[P, L])
+  /** Proxy that allows getting contents as a [[scala.Product]] type.
+    *
+    * Example usage:
+    * {{{
+    * case class Foo(x: Int, y: Double)
+    *
+    * val dataSheet = ...
+    *
+    * // Assuming dataSheet schema matches Foo
+    * dataSheet.as[Foo].head(10): Array[Foo]
+    * }}}
+    */
+  def as[P <: Product]: ProductProxy[P] = new ProductProxy[P] {}
 
-  def collectAsList[P <: Product]()(implicit Gen: Generic.Aux[P, L], L: FromTraversable[L]): List[P] =
-    dataFrame.collectAsList().asScala.toList.map(unsafeRowToProduct[P, L])
+  sealed abstract class ProductProxy[P <: Product] {
+    def collect[V <: HList]()(implicit Val: Values.Aux[Schema, V], Gen: Generic.Aux[P, V],
+                              P: ClassTag[P], V: FromTraversable[V]): Array[P] =
+      dataFrame.collect().map(unsafeRowToProduct[P, V])
 
-  def collectAsJavaList[P <: Product]()(implicit Gen: Generic.Aux[P, L], L: FromTraversable[L]): java.util.List[P] =
-    collectAsList().asJava
+    def collectAsScalaList[V <: HList]()(implicit Val: Values.Aux[Schema, V],Gen: Generic.Aux[P, V],
+                                         V: FromTraversable[V]): List[P] =
+      dataFrame.collectAsList().asScala.toList.map(unsafeRowToProduct[P, V])
 
-  def first[P <: Product]()(implicit Gen: Generic.Aux[P, L], L: FromTraversable[L]): P =
-    unsafeRowToProduct(dataFrame.first())
+    def collectAsList[V <: HList]()(implicit Val: Values.Aux[Schema, V], Gen: Generic.Aux[P, V],
+                                    V: FromTraversable[V]): java.util.List[P] =
+      collectAsScalaList[V].asJava
 
-  def flatMap[R](f: L => TraversableOnce[R])(implicit R: ClassTag[R], L: FromTraversable[L]): RDD[R] =
-    dataFrame.flatMap(f.compose(unsafeRowToHList[L]))
+    def first[V <: HList]()(implicit Val: Values.Aux[Schema, V], Gen: Generic.Aux[P, V], V: FromTraversable[V]): P =
+      unsafeRowToProduct(dataFrame.first())
 
-  def foreach(f: L => Unit)(implicit L: FromTraversable[L]): Unit =
-    dataFrame.foreach(f.compose(unsafeRowToHList[L]))
+    def head[V <: HList]()(implicit Val: Values.Aux[Schema, V], Gen: Generic.Aux[P, V], V: FromTraversable[V]): P =
+      unsafeRowToProduct(dataFrame.head())
 
-  def foreachPartition(f: Iterator[L] => Unit)(implicit L: FromTraversable[L]): Unit =
-    dataFrame.foreachPartition(f.compose(_.map(unsafeRowToHList[L])))
+    def head[V <: HList](n: Int)(implicit Val: Values.Aux[Schema, V], Gen: Generic.Aux[P, V],
+                                 P: ClassTag[P], V: FromTraversable[V]): Array[P] =
+      dataFrame.head(n).map(unsafeRowToProduct[P, V])
 
-  def head[P <: Product]()(implicit Gen: Generic.Aux[P, L], L: FromTraversable[L]): P =
-    unsafeRowToProduct(dataFrame.head())
+    def javaRDD[V <: HList](implicit Val: Values.Aux[Schema, V], Gen: Generic.Aux[P, V],
+                            V: FromTraversable[V]): JavaRDD[P] = {
+      val f = new JFunction[Row, P] { def call(v1: Row): P = unsafeRowToProduct(v1) }
+      dataFrame.javaRDD.map(f)
+    }
 
-  def head[P <: Product](n: Int)(implicit Gen: Generic.Aux[P, L], P: ClassTag[P], L: FromTraversable[L]): Array[P] =
-    dataFrame.head(n).map(unsafeRowToProduct[P, L])
+    def rdd[V <: HList](implicit Val: Values.Aux[Schema, V], Gen: Generic.Aux[P, V],
+                        P: ClassTag[P], V: FromTraversable[V]): RDD[P] =
+      dataFrame.rdd.map(unsafeRowToProduct[P, V])
 
-  def javaRDD[P <: Product](implicit Gen: Generic.Aux[P, L], L: FromTraversable[L]): JavaRDD[P] = {
-    val f = new JFunction[Row, P] { def call(v1: Row): P = unsafeRowToProduct(v1) }
-    dataFrame.javaRDD.map(f)
+    def take[V <: HList](n: Int)(implicit Val: Values.Aux[Schema, V], Gen: Generic.Aux[P, V],
+                                 P: ClassTag[P], V: FromTraversable[V]): Array[P] =
+      dataFrame.take(n).map(unsafeRowToProduct[P, V])
   }
 
-  def map[R](f: L => R)(implicit R: ClassTag[R], L: FromTraversable[L]): RDD[R] =
-    dataFrame.map(f.compose(unsafeRowToHList[L]))
+  /** Proxy that allows using combinators through [[shapeless.HList]] interface.
+    *
+    * Example usage:
+    * {{{
+    * val dataSheet: DataSheet[Int :: Double :: List[Byte] :: HNil] = ...
+    *
+    * def foo(i: Int, bs: List[Byte]): Long = ...
+    *
+    * dataSheet.combinator.map(hl => foo(hl(0), hl(2)))
+    * }}}
+    */
+  def combinator[V <: HList](implicit Val: Values.Aux[Schema, V]): CombinatorProxy[V] =
+    new CombinatorProxy[V] {}
 
-  def mapPartitions[R](f: Iterator[L] => Iterator[R])(implicit R: ClassTag[R], L: FromTraversable[L]): RDD[R] =
-    dataFrame.mapPartitions(f.compose(_.map(unsafeRowToHList[L])))
+  sealed abstract class CombinatorProxy[V <: HList] {
+    def flatMap[R](f: V => TraversableOnce[R])(implicit R: ClassTag[R], V: FromTraversable[V]): RDD[R] =
+      dataFrame.flatMap(f.compose(unsafeRowToHList[V]))
 
-  def rdd[P <: Product](implicit Gen: Generic.Aux[P, L], P: ClassTag[P], L: FromTraversable[L]): RDD[P] =
-    dataFrame.rdd.map(unsafeRowToProduct[P, L])
+    def foreach[R](f: V => Unit)(implicit V: FromTraversable[V]): Unit =
+      dataFrame.foreach(f.compose(unsafeRowToHList[V]))
 
-  def take[P <: Product](n: Int)(implicit Gen: Generic.Aux[P, L], P: ClassTag[P], L: FromTraversable[L]): Array[P] =
-    dataFrame.take(n).map(unsafeRowToProduct[P, L])
+    def foreachPartition(f: Iterator[V] => Unit)(implicit V: FromTraversable[V]): Unit =
+      dataFrame.foreachPartition(f.compose(_.map(unsafeRowToHList[V])))
+
+    def map[R](f: V => R)(implicit R: ClassTag[R], V: FromTraversable[V]): RDD[R] =
+      dataFrame.map(f.compose(unsafeRowToHList[V]))
+
+    def mapPartitions[R](f: Iterator[V] => Iterator[R])(implicit R: ClassTag[R], V: FromTraversable[V]): RDD[R] =
+      dataFrame.mapPartitions(f.compose(_.map(unsafeRowToHList[V])))
+  }
 }
 
 object DataSheet {
-  private def apply[L <: HList](dataFrame: DataFrame): DataSheet[L] =
-    new DataSheet[L](dataFrame)
+  private def apply[Schema <: HList](dataFrame: DataFrame): DataSheet[Schema] =
+    new DataSheet[Schema](dataFrame)
 
   private def unsafeRowToHList[L <: HList : FromTraversable](row: Row): L =
     row.toSeq.toHList[L].get
 
-  private def unsafeRowToProduct[P <: Product, L <: HList](row: Row)(implicit Gen: Generic.Aux[P, L], L: FromTraversable[L]): P =
+  private def unsafeRowToProduct[P <: Product, L <: HList](row: Row)(
+                                                           implicit Gen: Generic.Aux[P, L], L: FromTraversable[L]): P =
     Gen.from(unsafeRowToHList(row))
 
-  def fromRDD[P <: Product : TypeTag, L <: HList](rdd: RDD[P])(implicit Gen: Generic.Aux[P, L]): DataSheet[L] =
+  def fromRDD[P <: Product : TypeTag, Schema <: HList](rdd: RDD[P])(
+                                                       implicit Gen: LabelledGeneric.Aux[P, Schema]): DataSheet[Schema] =
     DataSheet(new SQLContext(rdd.sparkContext).implicits.rddToDataFrameHolder(rdd).toDF())
 }
