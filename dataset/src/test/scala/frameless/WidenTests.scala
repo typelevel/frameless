95e7179a0a4d042232be0fb4f5883123abd3f09e
Gleb Kanterov <gleb@kanterov.ru>
2016-10-23 23:47:08 +0200

    Add more implicits to `widen`

--- dataset/src/test/scala/frameless/WidenTests.scala
+++ dataset/src/test/scala/frameless/WidenTests.scala
@@ -28,12 +28,53 @@ class WidenTests extends TypedDatasetSuite {
       (rightGot ?= (sum :: Nil))
   }
 
+  def widen[A: TypedEncoder, B: TypedEncoder](a: A)(
+    implicit
+    view: A => B,
+    colView: TypedColumn[X1[A], A] => TypedColumn[X1[A], B]
+  ): Prop = {
+    val df = TypedDataset.create(X1(a) :: Nil)
+    val got = df.select(colView(df.col('a))).collect().run()
+
+    got ?= (view(a) :: Nil)
+  }
 
   {
     import frameless.implicits.widen._
 
     test("widen sum") {
       check(widenSum[Double, Int] _)
+    }
+
+    test("widen") {
+      implicit def byteToBigDecimal(x: Byte): BigDecimal = BigDecimal.valueOf(x.toLong)
+      implicit def shortToBigDecimal(x: Short): BigDecimal = BigDecimal.valueOf(x.toLong)
+
+      check(widen[Byte, Short] _)
+      check(widen[Byte, Int] _)
+      check(widen[Byte, Long] _)
+      check(widen[Byte, Double] _)
+      check(widen[Byte, BigDecimal] _)
+
+      check(widen[Short, Int] _)
+      check(widen[Short, Long] _)
+      check(widen[Short, Double] _)
+      check(widen[Short, BigDecimal] _)
+
+      check(widen[Int, Long] _)
+      check(widen[Int, Double] _)
+      check(widen[Int, BigDecimal] _)
+
+      check(widen[Long, Double] _)
+      check(widen[Long, BigDecimal] _)
+
+      check(widen[Double, BigDecimal] _)
+
+      // this is lawful (or not?) when we have Float
+      // check(widen[Byte, Float] _)
+      // check(widen[Short, Float] _)
+      // check(widen[Int, Float] _)
+      // check(widen[Long, Float] _)
     }
   }
 
