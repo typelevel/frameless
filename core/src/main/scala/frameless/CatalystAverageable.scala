2cab218f66fa74a08aa85ba8d8e7e10e6bcd7ea9
amine <amine.benhalloum@gmail.com>
2016-11-11 18:36:40 +0100

    make naming constistent with CatalystSummable

--- core/src/main/scala/frameless/CatalystAverageable.scala
+++ core/src/main/scala/frameless/CatalystAverageable.scala
@@ -8,9 +8,9 @@ package frameless
 trait CatalystAverageable[In, Out]
 
 object CatalystAverageable {
-  implicit val bigDecimalAveragable: CatalystAverageable[BigDecimal, BigDecimal] = new CatalystAverageable[BigDecimal, BigDecimal] {}
-  implicit val doubleAveragable: CatalystAverageable[Double, Double] = new CatalystAverageable[Double, Double] {}
-  implicit val LongAveragable: CatalystAverageable[Long, Double] = new CatalystAverageable[Long, Double] {}
-  implicit val IntAveragable: CatalystAverageable[Int, Double] = new CatalystAverageable[Int, Double] {}
-  implicit val ShortAveragable: CatalystAverageable[Short, Double] = new CatalystAverageable[Short, Double] {}
+  implicit val averageableBigDecimal: CatalystAverageable[BigDecimal, BigDecimal] = new CatalystAverageable[BigDecimal, BigDecimal] {}
+  implicit val averageableDouble: CatalystAverageable[Double, Double] = new CatalystAverageable[Double, Double] {}
+  implicit val averageableLong: CatalystAverageable[Long, Double] = new CatalystAverageable[Long, Double] {}
+  implicit val averageableInt: CatalystAverageable[Int, Double] = new CatalystAverageable[Int, Double] {}
+  implicit val averageableShort: CatalystAverageable[Short, Double] = new CatalystAverageable[Short, Double] {}
 }
