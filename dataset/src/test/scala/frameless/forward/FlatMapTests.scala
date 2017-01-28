671a33780736ff453504b14ab8fb66289a6c7663
Julien Truffaut <truffaut.julien@gmail.com>
2017-01-28 19:42:43 +0000

    fix FlatMapTests by using custom Arbitrary[Vector[A]]

--- dataset/src/test/scala/frameless/forward/FlatMapTests.scala
+++ dataset/src/test/scala/frameless/forward/FlatMapTests.scala
@@ -1,9 +1,14 @@
 package frameless
 
-import org.scalacheck.Prop
 import org.scalacheck.Prop._
+import org.scalacheck.{Arbitrary, Gen, Prop}
 
 class FlatMapTests extends TypedDatasetSuite {
+
+  // see issue with scalacheck non serializable Vector: https://github.com/rickynils/scalacheck/issues/315
+  implicit def arbVector[A](implicit A: Arbitrary[A]): Arbitrary[Vector[A]] =
+   Arbitrary(Gen.listOf(A.arbitrary).map(_.toVector))
+
   test("flatMap") {
     def prop[A: TypedEncoder, B: TypedEncoder](flatMapFunction: A => Vector[B], data: Vector[A]): Prop =
       TypedDataset.create(data).flatMap(flatMapFunction).collect().run().toVector =? data.flatMap(flatMapFunction)
