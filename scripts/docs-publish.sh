1b4d2d687eeb5f150ae4723c3a2514070f126409
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2017-05-22 10:04:00 +0200

    Update docs-publish.sh script

--- scripts/docs-publish.sh
+++ scripts/docs-publish.sh
@@ -2,14 +2,24 @@
 
 set -eux
 
+# Check that the working directory is a git repository and the repository has no outstanding changes.
+git diff-index --quiet HEAD
+
+commit=$(git show -s --format=%h)
+
 git checkout gh-pages
 
-git checkout master .
+git merge "$commit"
 
 bash scripts/docs-build.sh
 
 git add .
 
-git commit -am "Update book"
+git commit -am "Rebuild documentation ($commit)"
 
-echo "git push"
+echo "Verify that you didn't break anything:"
+echo "  $ python -m SimpleHTTPServer 8000"
+echo "  $ xdg-open http://localhost:8000/docs/book/"
+echo ""
+echo "Then push to the gh-pages branch:"
+echo "  $ git push gh-pages"
