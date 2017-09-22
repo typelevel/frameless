897e499649b80bb8e326267e66218593b1e788ac
Itamar Ravid <iravid@bigpanda.io>
2017-09-22 11:59:09 +0300

    Add a typed `col` function for creating column references
    Resolves #186.

--- dataset/src/main/scala/frameless/functions/package.scala
+++ dataset/src/main/scala/frameless/functions/package.scala
@@ -2,6 +2,8 @@ package frameless
 
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.apache.spark.sql.catalyst.expressions.Literal
+import org.apache.spark.sql.functions.{ col => sparkCol }
+import shapeless.Witness
 
 package object functions extends Udf with UnaryFunctions {
   object aggregate extends AggregateFunctions
@@ -16,5 +18,13 @@ package object functions extends Udf with UnaryFunctions {
       val expr = FramelessLit(value, encoder)
       new TypedColumn(expr)
     }
+  }
+
+  def col[T, A](column: Witness.Lt[Symbol])(
+    implicit
+    exists: TypedColumn.Exists[T, column.T, A],
+    encoder: TypedEncoder[A]): TypedColumn[T, A] = {
+    val untypedExpr = sparkCol(column.value.name).as[A](TypedExpressionEncoder[A])
+    new TypedColumn[T, A](untypedExpr)
   }
 }
