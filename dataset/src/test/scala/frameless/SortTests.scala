7fb80bfdac65945868de1af6085bd872326a3319
frosforever <frosforever@users.noreply.github.com>
2017-12-11 18:18:04 -0500

    add failing bad UDT test

--- dataset/src/test/scala/frameless/SortTests.scala
+++ dataset/src/test/scala/frameless/SortTests.scala
@@ -1,11 +1,63 @@
 package frameless
 
+import org.apache.spark.sql.FramelessInternals.UserDefinedType
+import org.apache.spark.sql.catalyst.util.ArrayBasedMapData
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{ functions => sfunc }
 import org.scalacheck.Prop
 import org.scalacheck.Prop._
 import shapeless.test.illTyped
 
+@SQLUserDefinedType(udt = classOf[UdtMapEncoded])
+class UdtMapClass(val a: Int) {
+  override def equals(other: Any): Boolean = other match {
+    case that: UdtMapClass => a == that.a
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq[Any](a)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
+
+  override def toString = s"UdtMapClass($a)"
+}
+
+object UdtMapClass {
+  implicit val urtEncoderClass = new UdtMapEncoded
+}
+
+class UdtMapEncoded extends UserDefinedType[UdtMapClass] {
+  override def sqlType: DataType = MapType(StringType, IntegerType)
+  override def serialize(obj: UdtMapClass): Any =
+    ArrayBasedMapData(Map.empty)
+  override def deserialize(datum: Any): UdtMapClass =
+    new UdtMapClass(1)
+  override def userClass: Class[UdtMapClass] = classOf[UdtMapClass]
+}
+
 class SortTests extends TypedDatasetSuite {
+  test("bad udt") {
+    /*
+    This is a UDT that from the outside looks like it only contains `Int`
+    but internally is represented by a `MapType`. This means that the generic
+    derivation will work from [[frameless.CatalystRowOrdered0]] but it'll blow
+    up at runtime due `MapType` not being sortable!
+
+    How can UDT be safely used here?
+     */
+    val ds = TypedDataset.create(Seq(Tuple1(new UdtMapClass(1))))
+
+    /*This will blow up at runtime!!
+      "due to data type mismatch: cannot sort data type udtmapencoded;"
+    */
+    ds.sort(ds('_1).asc).show().run()
+
+    //regular spark also blow up at runtime!
+    ds.dataset.sort(sfunc.col("_1"))
+  }
+
+
   test("prevent sorting by Map") {
     val ds = TypedDataset.create(Seq(
       X2(1, Map.empty[String, Int])
