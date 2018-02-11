cb9436379ac0de9ef7dc4c0268d35e246a08016f
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2018-02-11 10:38:36 +0100

    Add With type class
    With computes the intersection of two types while leaving less noise than A with B:
    
    - With[A, A] = A
    - With[A, B] = A with B (when A != B)
    
    This type function is needed to prevent IDEs from infering large types
    with shape `A with A with ... with A`. These types could be confusing for
    both end users and IDE's type checkers.

--- dataset/src/main/scala/frameless/TypedColumn.scala
+++ dataset/src/main/scala/frameless/TypedColumn.scala
@@ -671,3 +671,25 @@ object TypedColumn {
       ): Exists[T, K, V] = new Exists[T, K, V] {}
   }
 }
+
+/** Compute the intersection of two types:
+  *
+  * - With[A, A] = A
+  * - With[A, B] = A with B (when A != B)
+  *
+  * This type function is needed to prevent IDEs from infering large types
+  * with shape `A with A with ... with A`. These types could be confusing for
+  * both end users and IDE's type checkers.
+  */
+trait With[A, B] { type Out }
+
+trait LowPrioWith {
+  type Aux[A, B, W] = With[A, B] { type Out = W }
+  protected[this] val theInstance = new With[Any, Any] {}
+  protected[this] def of[A, B, W]: With[A, B] { type Out = W } = theInstance.asInstanceOf[Aux[A, B, W]]
+  implicit def identity[T]: Aux[T, T, T] = of[T, T, T]
+}
+
+object With extends LowPrioWith {
+  implicit def combine[A, B]: Aux[A, B, A with B] = of[A, B, A with B]
+}
