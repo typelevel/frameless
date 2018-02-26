ed5ae28b925fade29bd6a7316a7c734836e6e535
kmate <k_mate@inf.elte.hu>
2018-02-26 17:42:15 +0100

    Add tests for unit serialization

---
+++ dataset/src/test/scala/frameless/RecordEncoderTests.scala
@@ -0,0 +1,31 @@
+package frameless
+
+import org.scalatest.Matchers
+import shapeless.test.illTyped
+
+case class UnitsOnly(a: Unit, b: Unit)
+
+case class TupleWithUnits(u0: Unit, _1: Int, u1: Unit, u2: Unit, _2: String, u3: Unit)
+
+object TupleWithUnits {
+  def apply(_1: Int, _2: String): TupleWithUnits = TupleWithUnits((), _1, (), (), _2, ())
+}
+
+class RecordEncoderTests extends TypedDatasetSuite with Matchers {
+  test("Unable to encode only units") {
+    illTyped("""TypedEncoder[UnitsOnly]""")
+  }
+
+  test("Unit fields are skipped") {
+    assert(TypedEncoder[(Int, String)].catalystRepr == TypedEncoder[TupleWithUnits].catalystRepr)
+  }
+
+  test("") {
+    val df = session.createDataFrame(Seq((1, "one"), (2, "two")))
+    val ds = df.as[TupleWithUnits](TypedExpressionEncoder[TupleWithUnits])
+    val tds = TypedDataset.create(Seq(TupleWithUnits(1, "one"), TupleWithUnits(2, "two")))
+
+    df.collect shouldEqual tds.toDF.collect
+    ds.collect.toSeq shouldEqual tds.collect.run
+  }
+}
