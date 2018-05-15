3ed9db69193d7df332475d3238800017ae79c146
Rob Johnson <robert.johnson@sporting-advantage.co.uk>
2018-05-15 12:26:11 +0200

    Added mod operator to TypedColumn.scala

--- dataset/src/test/scala/frameless/NumericTests.scala
+++ dataset/src/test/scala/frameless/NumericTests.scala
@@ -59,8 +59,8 @@ class NumericTests extends TypedDatasetSuite {
     def prop[A: TypedEncoder: CatalystNumeric: Numeric](a: A, b: A)(implicit cd: CatalystDivisible[A, Double]): Prop = {
       val df = TypedDataset.create(X2(a, b) :: Nil)
       if (b == 0) proved else {
-        val div = implicitly[Numeric[A]].toDouble(a) / implicitly[Numeric[A]].toDouble(b)
-        val got = df.select(df.col('a) / df.col('b)).collect().run()
+        val div: Double = implicitly[Numeric[A]].toDouble(a) / implicitly[Numeric[A]].toDouble(b)
+        val got: Seq[Double] = df.select(df.col('a) / df.col('b)).collect().run()
 
         got ?= (div :: Nil)
       }
@@ -97,5 +97,54 @@ class NumericTests extends TypedDatasetSuite {
     }
 
     check(prop _)
+  }
+
+  trait NumericMod[T] {
+    def mod(a: T, b: T): T
+  }
+
+  object NumericMod {
+    implicit val byteInstance = new NumericMod[Byte] {
+      def mod(a: Byte, b: Byte) = (a % b).toByte
+    }
+    implicit val doubleInstance = new NumericMod[Double] {
+      def mod(a: Double, b: Double) = a % b
+    }
+    implicit val floatInstance = new NumericMod[Float] {
+      def mod(a: Float, b: Float) = a % b
+    }
+    implicit val intInstance = new NumericMod[Int] {
+      def mod(a: Int, b: Int) = a % b
+    }
+    implicit val longInstance = new NumericMod[Long] {
+      def mod(a: Long, b: Long) = a % b
+    }
+    implicit val shortInstance = new NumericMod[Short] {
+      def mod(a: Short, b: Short) = (a % b).toShort
+    }
+    implicit val bigDecimalInstance = new NumericMod[BigDecimal] {
+      def mod(a: BigDecimal, b: BigDecimal) = a % b
+    }
+  }
+
+  test("mod") {
+    import NumericMod._
+
+    def prop[A: TypedEncoder: CatalystNumeric: NumericMod](a: A, b: A): Prop = {
+      val df = TypedDataset.create(X2(a, b) :: Nil)
+      if (b == 0) proved else {
+        val mod: A = implicitly[NumericMod[A]].mod(a, b)
+        val got: Seq[A] = df.select(df.col('a) mod df.col('b)).collect().run()
+
+        got ?= (mod :: Nil)
+      }
+    }
+
+    check(prop[Byte] _)
+    check(prop[Double] _)
+    check(prop[Int   ] _)
+    check(prop[Long  ] _)
+    check(prop[Short ] _)
+    check(prop[BigDecimal] _)
   }
 }
