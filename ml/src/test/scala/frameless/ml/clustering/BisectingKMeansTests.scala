b626d8b276ef3810066ae6d7b273ad33248ea1f1
Bartek Tonia <btonia@virtuslab.com>
2018-09-20 11:30:46 +0200

    Bisecting-K-Means

---
+++ ml/src/test/scala/frameless/ml/clustering/BisectingKMeansTests.scala
@@ -0,0 +1,55 @@
+package frameless.ml.clustering
+
+import frameless.{TypedDataset, TypedEncoder, X1, X2, X3}
+import frameless.ml.classification.TypedBisectingKMeans
+import org.scalacheck.Arbitrary
+import org.scalatest.MustMatchers
+import org.apache.spark.ml.linalg._
+import org.scalacheck.Prop._
+import frameless.ml._
+
+class BisectingKMeansTests extends FramelessMlSuite with MustMatchers {
+  implicit val arbVectorNonEmpty: Arbitrary[Vector] =
+    Arbitrary(Generators.arbVector.arbitrary suchThat (_.size > 0))
+
+  test("fit() returns a correct TypedTransformer") {
+    val prop = forAll { x1: X1[Vector] =>
+      val km = TypedBisectingKMeans[X1[Vector]]()
+      val ds = TypedDataset.create(Seq(x1))
+      val model = km.fit(ds).run()
+      val pDs = model.transform(ds).as[X2[Vector, Int]]
+
+      pDs.select(pDs.col('a)).collect().run().toList == Seq(x1.a)
+
+    }
+
+    def prop3[A: TypedEncoder : Arbitrary] = forAll { x2: X2[Vector, A] =>
+      val km = TypedBisectingKMeans[X1[Vector]]
+      val ds = TypedDataset.create(Seq(x2))
+      val model = km.fit(ds).run()
+      val pDs = model.transform(ds).as[X3[Vector, A, Int]]
+
+      pDs.select(pDs.col('a), pDs.col('b)).collect.run() == Seq((x2.a, x2.b))
+    }
+
+    check(prop)
+    check(prop3[Double])
+  }
+
+  test("param setting is retained") {
+    val rf = TypedBisectingKMeans[X1[Vector]]()
+      .setK(10)
+      .setMaxIterations(10)
+      .setMinDivisibleClusterSize(1)
+      .setSeed(123332)
+
+    val ds = TypedDataset.create(Seq(X2(Vectors.dense(Array(0D)),0)))
+    val model = rf.fit(ds).run()
+
+      model.transformer.getK  == 10 &&
+      model.transformer.getMaxIter  == 10 &&
+      model.transformer.getMinDivisibleClusterSize  == 1 &&
+      model.transformer.getSeed == 123332
+
+  }
+}
--- ml/src/test/scala/frameless/ml/clustering/ClusteringIntegrationTests.scala
+++ ml/src/test/scala/frameless/ml/clustering/ClusteringIntegrationTests.scala
@@ -1,7 +1,7 @@
 package frameless.ml.clustering
 
 import frameless.ml.FramelessMlSuite
-import frameless.ml.classification.TypedKMeans
+import frameless.ml.classification.{TypedBisectingKMeans, TypedKMeans}
 import org.apache.spark.ml.linalg.Vector
 import org.scalatest.MustMatchers
 import frameless._
@@ -44,4 +44,40 @@ class ClusteringIntegrationTests extends FramelessMlSuite with MustMatchers {
 
     prediction mustEqual testSeq.map(_.field2)
   }
+
+  test("predict field2 from field1 using a bisecting K-means clustering") {
+    case class Data(field1: Double, field2: Int)
+
+    // Training
+
+    val trainingDataDs = TypedDataset.create(Seq.fill(5)(Data(10D, 0)) :+ Data(100D,0))
+
+    case class Features(field1: Double)
+    val vectorAssembler = TypedVectorAssembler[Features]
+
+    case class DataWithFeatures(field1: Double, field2: Int, features: Vector)
+    val dataWithFeatures = vectorAssembler.transform(trainingDataDs).as[DataWithFeatures]
+
+    case class BKMInputs(features: Vector)
+    val bkm = TypedBisectingKMeans[BKMInputs]().setK(2)
+
+    val model = bkm.fit(dataWithFeatures).run()
+
+    // Prediction
+    val testSeq = Seq(
+      Data(10D, 0),
+      Data(100D, 1)
+    )
+
+    val testData = TypedDataset.create(testSeq)
+    val testDataWithFeatures = vectorAssembler.transform(testData).as[DataWithFeatures]
+
+    case class PredictionResult(field1: Double, field2: Int, features: Vector, predictedField2: Int)
+    val predictionDs = model.transform(testDataWithFeatures).as[PredictionResult]
+
+    val prediction = predictionDs.select(predictionDs.col[Int]('predictedField2)).collect.run().toList
+
+    prediction mustEqual testSeq.map(_.field2)
+  }
+
 }
