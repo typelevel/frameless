23868dca3dfa7268e67cbcb40a00096be9f5e1d1
Chris Twiner <chris.twiner@gmail.com>
2020-07-06 12:17:02 +0200

    value is the new _1 and set by ExpressionEncoder, it's not possible to replace schema directly so some nullable checks just won't work

--- dataset/src/main/scala/frameless/TypedExpressionEncoder.scala
+++ dataset/src/main/scala/frameless/TypedExpressionEncoder.scala
@@ -1,5 +1,6 @@
 package frameless
 
+import org.apache.spark.sql.Encoder
 import org.apache.spark.sql.catalyst.analysis.GetColumnByOrdinal
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 import org.apache.spark.sql.catalyst.expressions.{BoundReference, CreateNamedStruct, If}
@@ -9,21 +10,19 @@ object TypedExpressionEncoder {
 
   /** In Spark, DataFrame has always schema of StructType
     *
-    * DataFrames of primitive types become records with a single field called "_1".
+    * DataFrames of primitive types become records with a single field called "value" set in ExpressionEncoder.
     */
   def targetStructType[A](encoder: TypedEncoder[A]): StructType = {
    encoder.catalystRepr match {
       case x: StructType =>
         if (encoder.nullable) StructType(x.fields.map(_.copy(nullable = true)))
         else x
-      case dt => new StructType().add("_1", dt, nullable = encoder.nullable)
+      case dt => new StructType().add("value", dt, nullable = encoder.nullable)
     }
   }
 
-  def apply[T: TypedEncoder]: ExpressionEncoder[T] = {
+  def apply[T: TypedEncoder]: Encoder[T] = {
     val encoder = TypedEncoder[T]
-    val targetSchema = targetStructType(encoder)
-
     val in = BoundReference(0, encoder.jvmRepr, encoder.nullable)
 
     val (out, serializer) = encoder.toCatalyst(in) match {
@@ -34,7 +33,7 @@ object TypedExpressionEncoder {
       case other =>
         val out = GetColumnByOrdinal(0, encoder.catalystRepr)
 
-        (out, other)
+       (out, other)
     }
 
     new ExpressionEncoder[T](
