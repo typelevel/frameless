87075ef1b66b67040a81eafd11aa2f9dfb6ce3c6
Tim Van Wassenhove <tim@timvw.be>
2020-10-28 05:16:19 +0100

    added test to verify the behavior of an option field

--- dataset/src/test/scala/frameless/ColumnViaLambdaTests.scala
+++ dataset/src/test/scala/frameless/ColumnViaLambdaTests.scala
@@ -3,15 +3,18 @@ package frameless
 import org.scalatest.matchers.should.Matchers
 import shapeless.test.illTyped
 
-case class MyClass1(a: Int, b: String, c: MyClass2)
+case class MyClass1(a: Int, b: String, c: MyClass2, g: Option[MyClass4])
 case class MyClass2(d: Long, e: MyClass3)
 case class MyClass3(f: Double)
+case class MyClass4(h: Boolean)
 
 class ColumnViaLambdaTests extends TypedDatasetSuite with Matchers {
 
-  def ds = TypedDataset.create(Seq(
-    MyClass1(1, "2", MyClass2(3L, MyClass3(7.0))),
-    MyClass1(4, "5", MyClass2(6L, MyClass3(8.0)))))
+  def ds = {
+    TypedDataset.create(Seq(
+      MyClass1(1, "2", MyClass2(3L, MyClass3(7.0)), Some(MyClass4(true))),
+      MyClass1(4, "5", MyClass2(6L, MyClass3(8.0)), None)))
+  }
 
   test("col(_.a)") {
     val col = ds.col(_.a)
@@ -53,6 +56,11 @@ class ColumnViaLambdaTests extends TypedDatasetSuite with Matchers {
     val actual = ds.select(col).collect.run()
     val expected = Seq(7.0, 8.0)
     actual shouldEqual expected
+  }
+
+  test("col(_.g.h does not compile") {
+    val col = ds.col(_.g) // the path "ends" at .g (can't access h)
+    illTyped("""ds.col(_.g.h)""")
   }
 
   test("col(_.a.toString) should not compile") {
