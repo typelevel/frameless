c8ecea8593cc7e9838e3e54257d35d6354703970
Chris Twiner <chris.twiner@gmail.com>
2023-06-05 13:41:02 +0200

    #343 - unpack to Literals - comment

--- dataset/src/main/scala/frameless/optimiser/LiteralRule.scala
+++ dataset/src/main/scala/frameless/optimiser/LiteralRule.scala
@@ -8,7 +8,7 @@ import org.apache.spark.sql.catalyst.rules.Rule
 object LiteralRule extends Rule[LogicalPlan] {
   override def apply(plan: LogicalPlan): LogicalPlan = plan.transformExpressions {
 
-    /**
+    /*
      * replace all literals to allow constant folding and push down
      */
     case Lit(dataType, _, _, convertedValue) =>
