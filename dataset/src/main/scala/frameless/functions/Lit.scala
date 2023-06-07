c6bbe2cff70d8a70674d16ca50239f858cae8f1d
Chris Twiner <chris.twiner@gmail.com>
2023-06-07 18:28:53 +0200

    #343 - bring back code gen with lazy to stop recompiles

--- dataset/src/main/scala/frameless/functions/Lit.scala
+++ dataset/src/main/scala/frameless/functions/Lit.scala
@@ -13,8 +13,46 @@ private[frameless] case class Lit[T <: AnyVal](
 ) extends Expression with NonSQLExpression {
   override def toString: String = s"FramelessLit(${show()})"
 
-  def eval(input: InternalRow): Any = catalystExpr.eval(input)
+  lazy val codegen = {
+    val ctx = new CodegenContext()
+    val eval = genCode(ctx)
 
+    val codeBody =
+      s"""
+      public scala.Function1<InternalRow, Object> generate(Object[] references) {
+        return new LiteralEvalImpl(references);
+      }
+
+      class LiteralEvalImpl extends scala.runtime.AbstractFunction1<InternalRow, Object> {
+        private final Object[] references;
+        ${ctx.declareMutableStates()}
+        ${ctx.declareAddedFunctions()}
+
+        public LiteralEvalImpl(Object[] references) {
+          this.references = references;
+          ${ctx.initMutableStates()}
+        }
+
+        public java.lang.Object apply(java.lang.Object z) {
+          InternalRow ${ctx.INPUT_ROW} = (InternalRow) z;
+          ${eval.code}
+          return ${eval.isNull} ? ((Object)null) : ((Object)${eval.value});
+        }
+      }
+    """
+
+    val code = CodeFormatter.stripOverlappingComments(
+      new CodeAndComment(codeBody, ctx.getPlaceHolderToComments())
+    )
+
+    val (clazz, _) = CodeGenerator.compile(code)
+    val codegen =
+      clazz.generate(ctx.references.toArray).asInstanceOf[InternalRow => AnyRef]
+    codegen
+  }
+
+  def eval(input: InternalRow): Any = codegen(input)
+  
   def children: Seq[Expression] = Nil
 
   protected def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = catalystExpr.genCode(ctx)
