e9999c1043bac86b159c59b82652b29dc878e001
Chris Twiner <chris.twiner@gmail.com>
2023-06-07 11:48:43 +0200

    #343 - added the struct test back

--- dataset/src/test/scala/frameless/sql/rules/FramelessLitRuleTests.scala
+++ dataset/src/test/scala/frameless/sql/rules/FramelessLitRuleTests.scala
@@ -7,8 +7,10 @@ import frameless.sql.FramelessOptimizations
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.catalyst.util.DateTimeUtils.{currentTimestamp, microsToInstant}
-import org.apache.spark.sql.sources.{GreaterThanOrEqual, IsNotNull}
+import org.apache.spark.sql.sources.{GreaterThanOrEqual, IsNotNull, EqualTo}
 import org.apache.spark.sql.catalyst.expressions
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.types.StructType
 
 import java.time.Instant
 
@@ -45,12 +47,39 @@ trait FramelessLitRuleTests extends SQLRulesSuite {
 class FramelessLitOptimizationRuleTests extends FramelessLitRuleTests {
   override def registerOptimizations(sqlContext: SQLContext): Unit =
     LiteralRule.registerOptimizations(sqlContext)
+
+  test("struct pushdown should not work") {
+    type Payload = X4[Int, Int, Int, Int]
+    val expectedStructure = X1(X4(1, 2, 3, 4))
+    val expectedPushDownFilters = List(IsNotNull("a"))
+
+    optimizationRulesTest[Payload](
+      expectedStructure,
+      expectedPushDownFilters,
+      { case e@expressions.EqualTo(_, _: Lit[_]) => e },
+      _ === expectedStructure.a
+    )
+  }
 }
 
 class FramelessLitExtensionsTests extends FramelessLitRuleTests {
   override def addSparkConfigProperties(config: SparkConf): Unit = {
     config.set("spark.sql.extensions", classOf[FramelessOptimizations].getName)
     ()
+  }
+
+  test("struct pushdown") {
+    type Payload = X4[Int, Int, Int, Int]
+    val expectedStructure = X1(X4(1, 2, 3, 4))
+    val expected = new GenericRowWithSchema(Array(1, 2, 3, 4), implicitly[TypedEncoder[Payload]].catalystRepr.asInstanceOf[StructType])
+    val expectedPushDownFilters = List(IsNotNull("a"), EqualTo("a", expected))
+
+    optimizationRulesTest[Payload](
+      expectedStructure,
+      expectedPushDownFilters,
+      { case e@expressions.EqualTo(_, _: Lit[_]) => e },
+      _ === expectedStructure.a
+    )
   }
 }
 
